4f54e862a5979922ffc3b3c8b231a174a3529c73
S.P.Chandra Sai <spchandrasai820@gmail.com>
2025-03-12 01:43:36 +0530

    shadcnUI with Nextjs

--- Lecture_2_shadcn_intro/shadcnintro/src/app/login/page.jsx
+++ Lecture_2_shadcn_intro/shadcnintro/src/app/login/page.jsx
@@ -7,24 +7,27 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 
 
 function Login() {
     return (
-        <div>
-            <Card>
+        <div className="h-[100vh] flex justify-center items-center">
+            <Card className="h-[450px] w-[400px] bg-[rgb(28, 25, 23)]">
                 <CardHeader>
-                    <CardTitle>Card Title</CardTitle>
-                    <CardDescription>Card Description</CardDescription>
+                    <CardTitle className="text-white text-4xl">Login</CardTitle>
+                    <CardDescription className="text-md">Enter your email below to login to your account.</CardDescription>
                 </CardHeader>
                 <CardContent>
-                    <p>Card Content</p>
+                    <Input type="email" placeholder="M@gmail.com"></Input>
+                    <Button variant={"destructive"} className="w-full">Login</Button>
                 </CardContent>
                 <CardFooter>
-                    <p>Card Footer</p>
+                    <p>Forgot Password</p>
+                    <p>Need an account</p>
                 </CardFooter>
             </Card>
-
         </div>
     )
 }
