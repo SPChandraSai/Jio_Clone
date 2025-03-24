8a76048fde9161b892bf391ffe72ffbd044b750e
S.P.Chandra Sai <spchandrasai820@gmail.com>
2025-03-24 16:11:44 +0530

    redux-2 middleWare pending

--- Lecture_3_redux_toolkit/3_redux/redux_example/src/App.jsx
+++ Lecture_3_redux_toolkit/3_redux/redux_example/src/App.jsx
@@ -2,12 +2,14 @@ import NormalCounter from './components/NormalCounter';
 import NormalInputCounter from './Components/NormalInputCounter';
 import ReduxCounter from './Components/ReduxCounter';
 import ReduxInputCounter from './Components/ReduxInputCounter';
+import UserComponent from './Components/User';
 function App() {
   return (
     <>
       {/* <ReduxCounter /> */}
       {/* <NormalInputCounter /> */}
-      <ReduxInputCounter/>
+      {/* <ReduxInputCounter/> */}
+      <UserComponent/>
     </>
   )
 }
--- Lecture_3_redux_toolkit/3_redux/redux_example/src/redux/store.js
+++ Lecture_3_redux_toolkit/3_redux/redux_example/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./slice/CounterSlics.js";
 import counterInputSlice from "./slice/CounterInputSlice.js";
+import userSlice from "./slice/UserSlice.js";
 const store = configureStore({
     reducer: {
         counterSection: counterSlice.reducer,
         counterInputSlice: counterInputSlice.reducer,
+        userSection:userSlice.reducer,
     }
 })
 export default store;
