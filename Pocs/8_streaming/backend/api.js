848a46a6dd3ab8a6234eb075097120c520c2ccfd
S.P.Chandra Sai <spchandrasai820@gmail.com>
2025-04-18 15:19:53 +0530

    streaming with range(chunk size)

--- Pocs/8_streaming/backend/api.js
+++ Pocs/8_streaming/backend/api.js
@@ -14,12 +14,44 @@ app.get("/memoryIntensive", function (req, res) {
 app.get("/streamfile", function (req, res) {
     console.log("file readStream created");
     // const fileStreamInstance = fs.createReadStream("Amreen 1.0.fig.zip");
-    const videoStreamInstance = fs.createReadStream("Tom And Jerry Tales - S02 E01")
+    const videoStreamInstance = fs.createReadStream("video1")
     // fileStreamInstance.pipe(res);
     res.writeHead(200, {
         "content-type": "video/mp4"
     })
     videoStreamInstance.pipe(res);
+})
+
+app.get("/rangestreaming", function (req, res) {
+    // get the range from the request header -> video player
+    const range = req.headers.range;
+    if (range) {
+        const stat = fs.statSync("video1.mp4");
+        const fileSize = stat.size;
+        const parts = range.replace(/bytes=/, "").split("-");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+        const chunkSize = 10 ** 6; // 1MB
+        // console.log("chunk size", chunkSize);
+        const header = {
+            "Content-Type": "video/mp4",
+            "Content-Length": chunkSize,
+            "Accept-Ranges": "bytes",
+            "Content-range": `bytes ${start}-${end}/${fileSize}`
+        }
+        // send a 206 partial content status
+        res.writeHead(206, header);
+        // create a read stream for the video file
+        const file = fs.createReadStream("video1.mp4", { start, end });
+        // pipe the file stream to the response
+        file.pipe(res);
+    }
+    else {
+        req.status(400).json({
+            message: "Invalid request"
+        })
+    }
 })
 
 app.listen(3000, function () {
--- Pocs/8_streaming/frontend/src/App.jsx
+++ Pocs/8_streaming/frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <h2>Streaming Video Player</h2>
       <video controls height={500}>
-        <source src="https://localhost:3000/streamfile" type='video/mp4'/>
+        <source src="http://localhost:3000/rangestreaming" type='video/mp4'/>
       </video>
     </>
   )
